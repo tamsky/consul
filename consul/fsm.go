389326aa2fa855736290cf0533d20b2672a19597
James Phillips <james@hashicorp.com>
2015-10-12 15:34:32 -0700

    Integrates new state store into session endpoint; returns table index always.

--- consul/fsm.go
+++ consul/fsm.go
@@ -78,7 +78,7 @@ func (c *consulFSM) Close() error {
 	return c.state.Close()
 }
 
-// TODO(slackpad)
+// StateNew is used to return a handle to the current state
 func (c *consulFSM) StateNew() *state.StateStore {
 	return c.stateNew
 }
